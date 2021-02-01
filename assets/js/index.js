225bef2103ddd1b8745319bca915d22436b62e7f
jh Lim <jhwlim25@gmail.com>
2021-02-01 09:27:33 +0900

    네비게이션 반응형 추가 및 css/js 파일 정리

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,90 +1,21 @@
-$(document).ready(function() {
+// About-Skills 가 viewport에 들어왔을 때 확인하기
+function elementInViewPort(el) {
+    var top = el.offsetTop
 
-    $('#headerBtn').click(() => {
-        location.href="#about";
-    });
-
-    // // 헤더 버튼 hover 이벤트
-    $('#headerBtn').mouseover(function() {
-        $(this).html('구경하러가기');
-    });
-    $('#headerBtn').mouseout(function() {
-        $(this).text("궁금하신가요?");
-    }); 
-    
-    // About-Skills 가 viewport에 들어왔을 때 확인하기
-    function elementInViewPort(el) {
-        var top = el.offsetTop
-
-        while(el.offsetParent) {
-            el = el.offsetParent;
-            top += el.offsetTop;
-        }
-        console.log(top);
-        return top >= window.pageYOffset;
+    while(el.offsetParent) {
+        el = el.offsetParent;
+        top += el.offsetTop;
     }
-
-    // $(window).scroll(function() {
-    //     var el = $('#aboutSkillBox');
-    //     var top = el.offset().top;
-        
-    //     var windowTop = $(this).scrollTop();
-    //     var windowHeight = $(this).height();
-
-    //     if (top > windowTop && top < windowTop + windowHeight) {
-    //         $('.skill-bar__fill').addClass('fill-bar');
-    //     }
-
-    //     var skillSection = $("#skill");
-    //     var topOfSkillSection = skillSection.offset().top;
-    //     var bottomOfSkillSection = topOfSkillSection + skillSection.height() + 40;
-    //     var navHeight = $('nav').height();
-    //     if (windowTop >= topOfSkillSection - navHeight && windowTop <= bottomOfSkillSection) {
-    //         $('#navigator').css('background-color', 'rgba(255, 255, 255, 0.5)');
-    //         $('#navigator').css('color', 'rgba(37, 42, 52, 1)');
-    //     } else {
-    //         $('#navigator').css('background-color', 'rgba(37, 42, 52, 1)');
-    //         $('#navigator').css('color', 'white');
-    //     }
-    // });
-
-    
-
-    $('#aboutSkillAll').click(function() {
-        $('.skill__hex-outer--shadow').removeClass('hex-rotateY');
-        $('.skill__hex-outer--shadow').removeClass('hex-blur');
-        $(".skill-bar__fill").removeClass('fill-bar');
-        setTimeout(function() {
-            $(".skill-bar__fill").addClass('fill-bar');
-        }, 1);
-
-        $('.skill-bar__text').hide();    
-        
-        $(".skill-bar__row").show();
-    });
-    $('#aboutSkillFront').click(function() {
-        $('.skill__hex-outer--shadow').removeClass('hex-blur');
-        $('.not-frontend').addClass('hex-blur');
-    });
-    $('#aboutSkillBack').click(function() {
-        $('.skill__hex-outer--shadow').removeClass('hex-blur');
-        $('.not-backend').addClass('hex-blur');
-    });
-
-    $('.skill__selector').click(function() {
-        if (!$(this).hasClass('skill__selector--selected')) {
-            $('.skill__selector').removeClass('skill__selector--selected');
-            $(this).addClass('skill__selector--selected');
-            
-        }
-    });
-
-    // $('.portfolio__figure').mouseover(function() {
-    //     $(this).children('.portfolio__description').addClass('portfolio__description--display');
-    // });
-
-    // $('.portfolio__figure').mouseout(function() {
-    //     $(this).children('.portfolio__description').removeClass('portfolio__description--display');
-    // });
-
-});
+    console.log(top);
+    return top >= window.pageYOffset;
+}
+function updateSkillBarText(labelFor) {
+    var textArr = skillBarTexts[labelFor];
+    var skillBarText = document.getElementById('skillBarText'); 
+    skillBarText.innerHTML = "";
+    for (i in textArr) {
+        var listItem = document.createElement('li');
+        listItem.innerText = textArr[i];
+        skillBarText.appendChild(listItem);
+    }
+}
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,36 @@
+const portfolios = {
+    portfolio1 : {
+        title : "사진 관리 어플리케이션",
+        imgs : [
+            './assets/images/portfolio1_1.png',
+            './assets/images/portfolio1_2.png',
+            './assets/images/portfolio1_3.png',
+        ],
+        summary : '사진을 모아주고 정리하는 것을 도와주는 어플리케이션',
+        role : [
+            'RemoteURL로 영상을 받을 때 로컬 URL을 UserDefaults에 저장해 다시 다운로드 받지 않도록 구현',
+            'Player를 보여주는 부분과 제어하는 부분을 나눔',
+            '각각의 뷰를 관리하는 Manager 클래스를 두어 컨트롤러가 Manager와 소통',
+            '제스처를 이용한 플레이어 제어'
+        ],
+        skill : 'Swift 3, Xcode 8',
+        gitRepo : 'https://github.com/jhwlim',
+    },
+    portfolio2 : {
+        title : "Vlogr",
+        imgs : [
+            './assets/images/portfolio1_4.png',
+            './assets/images/portfolio1_5.png',
+        ],
+        summary : '일상을 전달하는 Vlog 소셜 네트워크',
+        role : [
+            '재사용되는 코드를 묶어서 관리',
+            '동영상 전체화면으로 전환시 UIModalPresentationStyle의 overFullScreen을 적용해 아래의 뷰가 자연스럽게 흐려지도록 구현.',
+        ],
+        skill : 'Objective-C, Swift, Xcode 9',
+        gitRepo : 'https://github.com'
+    },
+}
 document.addEventListener("DOMContentLoaded", function() {
     const portfolioModal = document.getElementById('portfolioModal');
     portfolioModal.addEventListener('show.bs.modal', function (event) {
@@ -71,37 +104,3 @@ document.addEventListener("DOMContentLoaded", function() {
         target.appendChild(a);
     }
 });
-const portfolios = {
-    portfolio1 : {
-        title : "사진 관리 어플리케이션",
-        imgs : [
-            './assets/images/portfolio1_1.png',
-            './assets/images/portfolio1_2.png',
-            './assets/images/portfolio1_3.png',
-        ],
-        summary : '사진을 모아주고 정리하는 것을 도와주는 어플리케이션',
-        role : [
-            'RemoteURL로 영상을 받을 때 로컬 URL을 UserDefaults에 저장해 다시 다운로드 받지 않도록 구현',
-            'Player를 보여주는 부분과 제어하는 부분을 나눔',
-            '각각의 뷰를 관리하는 Manager 클래스를 두어 컨트롤러가 Manager와 소통',
-            '제스처를 이용한 플레이어 제어'
-        ],
-        skill : 'Swift 3, Xcode 8',
-        gitRepo : 'https://github.com/jhwlim',
-    },
-    portfolio2 : {
-        title : "Vlogr",
-        imgs : [
-            './assets/images/portfolio1_4.png',
-            './assets/images/portfolio1_5.png',
-        ],
-        summary : '일상을 전달하는 Vlog 소셜 네트워크',
-        role : [
-            '재사용되는 코드를 묶어서 관리',
-            '동영상 전체화면으로 전환시 UIModalPresentationStyle의 overFullScreen을 적용해 아래의 뷰가 자연스럽게 흐려지도록 구현.',
-        ],
-        skill : 'Objective-C, Swift, Xcode 9',
-        gitRepo : 'https://github.com'
-    },
-      
-}
